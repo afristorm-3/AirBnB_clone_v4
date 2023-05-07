15d852b7cc05b0b543bd4a073fca69aacdf433a2
afristorm-3 <afristorm3@gmail.com>
2023-05-07 19:52:33 -0400

    update fix

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+  // task 3
+  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
